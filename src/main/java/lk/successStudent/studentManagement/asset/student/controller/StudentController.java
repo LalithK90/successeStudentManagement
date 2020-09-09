1be223619b521f478903c0d7ddc18d731ce4a62f
LalithK90 <asakahatapitiya@gmail.com>
2020-09-09 21:46:06 +0530

    Student reg number was completed

--- src/main/java/lk/successStudent/studentManagement/asset/student/controller/StudentController.java
+++ src/main/java/lk/successStudent/studentManagement/asset/student/controller/StudentController.java
@@ -4,6 +4,7 @@ package lk.successStudent.studentManagement.asset.student.controller;
 import lk.successStudent.studentManagement.asset.student.entity.Student;
 import lk.successStudent.studentManagement.asset.student.service.StudentService;
 import lk.successStudent.studentManagement.util.interfaces.AbstractController;
+import lk.successStudent.studentManagement.util.service.MakeAutoGenerateNumberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,12 +14,15 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/student")
-public class StudentController implements AbstractController<Student, Integer> {
+@RequestMapping( "/student" )
+public class StudentController implements AbstractController< Student, Integer > {
     private final StudentService studentService;
+    private final MakeAutoGenerateNumberService makeAutoGenerateNumberService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService,
+                             MakeAutoGenerateNumberService makeAutoGenerateNumberService) {
         this.studentService = studentService;
+        this.makeAutoGenerateNumberService = makeAutoGenerateNumberService;
     }
 
     @GetMapping
@@ -27,34 +31,35 @@ public class StudentController implements AbstractController<Student, Integer> {
         return "student/student";
     }
 
-    @GetMapping("/new")
+    @GetMapping( "/new" )
     public String form(Model model) {
         model.addAttribute("student", new Student());
 
-        model.addAttribute("addStatus",true);
+        model.addAttribute("addStatus", true);
         return "student/addStudent";
     }
 
-    @GetMapping("/view/{id}")
+    @GetMapping( "/view/{id}" )
     public String findById(@PathVariable Integer id, Model model) {
         model.addAttribute("studentDetail", studentService.findById(id));
         return "student/student-detail";
     }
 
-    @GetMapping("/edit/{id}")
+    @GetMapping( "/edit/{id}" )
     public String edit(@PathVariable Integer id, Model model) {
         model.addAttribute("student", studentService.findById(id));
 
-        model.addAttribute("addStatus",false);
+        model.addAttribute("addStatus", false);
         return "student/addStudent";
     }
 
-    @PostMapping("/save")
-    public String persist(@Valid @ModelAttribute Student student, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-        if (bindingResult.hasErrors()) {
+    @PostMapping( "/save" )
+    public String persist(@Valid @ModelAttribute Student student, BindingResult bindingResult,
+                          RedirectAttributes redirectAttributes, Model model) {
+        if ( bindingResult.hasErrors() ) {
             model.addAttribute("student", student);
 
-            model.addAttribute("addStatus",true);
+            model.addAttribute("addStatus", true);
             return "student/addStudent";
         }
 
@@ -81,7 +86,7 @@ public class StudentController implements AbstractController<Student, Integer> {
 
     }
 
-    @GetMapping("/delete/{id}")
+    @GetMapping( "/delete/{id}" )
     public String delete(@PathVariable Integer id, Model model) {
         studentService.delete(id);
         return "redirect:/student";
