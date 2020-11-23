7a846c04f6356aeb2f3611d6648fa657203b52e8
LalithK90 <asakahatapitiya@gmail.com>
2020-11-23 20:53:22 +0530

    Gradel enum created and add to subject

---
+++ src/main/java/lk/succes/student_management/asset/batch/entity/enums/Grade.java
@@ -0,0 +1,13 @@
+package lk.succes.student_management.asset.batch.entity.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Grade {
+  GRADE("6");
+
+  private final String grade;
+
+}
