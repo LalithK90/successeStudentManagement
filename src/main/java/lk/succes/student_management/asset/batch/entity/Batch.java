79480c3a2a92c7404a0888df94560c3b98b8a060
LalithK90 <asakahatapitiya@gmail.com>
2020-11-23 21:03:08 +0530

    Batch  was fixed

--- src/main/java/lk/succes/student_management/asset/batch/entity/Batch.java
+++ src/main/java/lk/succes/student_management/asset/batch/entity/Batch.java
@@ -6,6 +6,7 @@ import lk.succes.student_management.asset.batch.entity.enums.Grade;
 import lk.succes.student_management.asset.hall.entity.Hall;
 import lk.succes.student_management.asset.student.entity.Student;
 import lk.succes.student_management.asset.subject.entity.Subject;
+import lk.succes.student_management.asset.teacher.entity.Teacher;
 import lk.succes.student_management.util.audit.AuditEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,14 +25,17 @@ import java.util.List;
 public class Batch extends AuditEntity {
 
     private String classReferance;
+
+    private String year;
+
     @Enumerated(EnumType.STRING)
     private Grade grade;
-    private String year;
-    private String subjectName;
-    private String teacherName;
 
     @ManyToOne
     private Subject subject;
+
+    @ManyToOne
+    private Teacher teacher;
 
 //so many student on one batch
     @OneToMany(mappedBy = "batch")
