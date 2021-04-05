db59b025a7d0e3874232c95cf48356c122104a80
LalithK90 <asakahatapitiya@gmail.com>
2021-04-05 23:13:47 +0530

    payment issue was fixed

--- src/main/java/lk/succes_student_management/asset/payment/dao/PaymentDao.java
+++ src/main/java/lk/succes_student_management/asset/payment/dao/PaymentDao.java
@@ -3,6 +3,7 @@ package lk.succes_student_management.asset.payment.dao;
 
 import lk.succes_student_management.asset.batch_student.entity.BatchStudent;
 import lk.succes_student_management.asset.payment.entity.Payment;
+import lk.succes_student_management.asset.payment.entity.enums.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +15,6 @@ public interface PaymentDao extends JpaRepository<Payment, Integer> {
   Payment findFirstByOrderByIdDesc();
 
   Payment findByBatchStudentAndMonth(BatchStudent batchStudent, Month month);
+
+  Payment findByMonthAndBatchStudentAndPaymentStatus(BatchStudent batchStudent, Month month, PaymentStatus noPaid);
 }
